a2bd9c2112ec0d75ac03f173d389bb7332b46c8d
Ali Abbas <ali_abbas89@hotmail.com>
2014-12-23 12:55:15 +0500

    testCheckin

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+
+
+
+
+        }
+    }
+}
